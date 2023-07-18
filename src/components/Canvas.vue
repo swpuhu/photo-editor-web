96c139982b3ee9eef919e4af10c01f88a8f28d03
huyunhe <huyunhe@sparkedu.com>
2023-07-18 18:29:21 +0800

    refactor scene

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
-import { Camera, Mesh, SimpleEngine } from '../../submodule/renderer/index';
+import { onMounted, ref } from 'vue';
+import {
+    Camera,
+    Mesh,
+    Node,
+    SimpleEngine,
+} from '../../submodule/renderer/index';
 import { Geometry } from '../../submodule/renderer';
 import { Scene } from '../../submodule/renderer/Scene';
-import { Node } from '../../submodule/renderer';
-import { UnLitMaterial } from '../../submodule/renderer';
+import { Sprite } from '../../submodule/renderer/script/Sprite';
 import { useGlobalStore } from '../store/GlobalStore';
-import { Texture } from '../../submodule/renderer/Texture';
+
+import { Node2D } from '../../submodule/renderer/Node2D';
+import { Event } from '../../submodule/renderer/Event';
+import { TouchEvent } from '../../submodule/renderer/Event';
+import { SpriteDefaultMaterial } from '../../submodule/renderer/material/SpriteDefaultMaterial';
 
 let engine: SimpleEngine;
-let texture: Texture;
+let sprite: Sprite;
 let geo: Geometry;
 let canvasDom: HTMLCanvasElement;
 let rootDom: HTMLElement;
@@ -20,6 +28,10 @@ const styleWidth = ref(0);
 const styleHeight = ref(0);
 const styleLeft = ref(0);
 const styleTop = ref(0);
+
+let parentWidth = 0;
+let parentHeight = 0;
+let imgDisplayNode: Node2D;
 
 const computeWidth = (): number => {
     if (!rootDom) {
@@ -71,47 +83,71 @@ globalStore.$subscribe(async () => {
         return;
     }
     const currentUrl = globalStore.currentImg;
-    if (currentUrl) {
-        await texture.loadTexture(currentUrl);
-        canvasDom.width = texture.width;
-        canvasDom.height = texture.height;
-        aspect.value = canvasDom.width / canvasDom.height;
-        styleWidth.value = computeWidth();
-        styleHeight.value = computeHeight();
-        computeStyleLeft();
-        computeStyleTop();
-        engine.setViewSize(texture.width, texture.height);
+    if (currentUrl && imgDisplayNode) {
+        await sprite.setURL(currentUrl);
+        const spriteAsp = sprite.rawWidth / sprite.rawHeight;
+        const canvasAsp = canvasDom.width / canvasDom.height;
+        if (spriteAsp > canvasAsp) {
+            // adapt to width
+            imgDisplayNode.width = canvasDom.width;
+            imgDisplayNode.height = canvasDom.width / spriteAsp;
+        } else {
+            imgDisplayNode.height = canvasDom.height;
+            imgDisplayNode.width = canvasDom.height * spriteAsp;
+        }
+        // engine.setViewSize(sprite.rawWidth, sprite.rawHeight);
+
+        // aspect.value = canvasDom.width / canvasDom.height;
+        // styleWidth.value = computeWidth();
+        // styleHeight.value = computeHeight();
+        // computeStyleLeft();
+        // computeStyleTop();
     }
 });
 
-onMounted(() => {
-    canvasDom = canvasRef.value as unknown as HTMLCanvasElement;
+const initScene = () => {
     rootDom = rootRef.value as unknown as HTMLElement;
-    const gl = canvasDom.getContext('webgl');
+    canvasDom = canvasRef.value as unknown as HTMLCanvasElement;
+
+    parentWidth = rootDom.parentElement?.clientWidth!;
+    parentHeight = rootDom.parentElement?.clientHeight!;
+    canvasDom.width = parentWidth;
+    canvasDom.height = parentHeight;
+    const gl = canvasDom.getContext('webgl2');
     const width = canvasDom.width;
     const height = canvasDom.height;
-    engine = new SimpleEngine(gl);
+    console.log(width, height);
+    engine = new SimpleEngine(gl!);
     geo = Geometry.getQuad(width, height);
     const scene = new Scene('scene');
     const root = new Node('root');
-    texture = new Texture();
-    const mat = new UnLitMaterial(texture);
+    root.x = parentWidth / 2;
+    root.y = parentHeight / 2;
+    imgDisplayNode = new Node2D('img');
+    imgDisplayNode.width = 100;
+    imgDisplayNode.height = 100;
+    sprite = imgDisplayNode.addScript(Sprite);
+    root.addChildren(imgDisplayNode);
 
-    const cam = new Camera(
-        -width / 2,
-        width / 2,
-        -height / 2,
-        height / 2,
-        -100,
-        100,
-        'orthoCam'
-    );
+    imgDisplayNode.on(Event.TOUCH, (e: TouchEvent) => {
+        console.log('click', e);
+    });
 
-    new Mesh(geo, mat, root);
+    root.on(Event.TOUCH, (e: TouchEvent) => {
+        console.log('click from root');
+    });
+    const mat = new SpriteDefaultMaterial();
+    const cam = new Camera(0, width, 0, height, -100, 100, 'orthoCam');
+    // new Mesh(geo, mat, root);
     scene.addChildren(root);
+
     scene.addChildren(cam);
     engine.setScene(scene);
     engine.run();
+};
+
+onMounted(() => {
+    initScene();
 });
 </script>
 
@@ -120,10 +156,10 @@ onMounted(() => {
         <canvas
             class="canvas"
             :style="{
-                width: styleWidth + 'px',
-                height: styleHeight + 'px',
-                left: styleLeft + 'px',
-                top: styleTop + 'px',
+                // width: styleWidth + 'px',
+                // height: styleHeight + 'px',
+                // left: styleLeft + 'px',
+                // top: styleTop + 'px',
             }"
             :width="500"
             :height="500"
