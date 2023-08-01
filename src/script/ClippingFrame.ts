5f491dda3fbec33228ef0be2d0d781ff9c02c1c4
huyunhe <huyunhe@sparkedu.com>
2023-08-01 19:02:49 +0800

    add line

--- src/script/ClippingFrame.ts
+++ src/script/ClippingFrame.ts
@@ -4,12 +4,16 @@ import { Node2D } from '@/simple-render-engine/renderer/Node2D';
 import { SolidColorMaterial } from '@/simple-render-engine/renderer/material/SolidColorMaterial';
 import { EngineScript } from '@/simple-render-engine/renderer/script/EngineScript';
 import { SolidColor } from '@/simple-render-engine/renderer/script/SolidColor';
-
+const DEFAULT_LINE_WIDTH = 2;
 export class ClippingFrame extends EngineScript {
     private __leftTopCtr: Node2D | null = null;
     private __rightTopCtr: Node2D | null = null;
     private __leftBottomCtr: Node2D | null = null;
     private __rightBottomCtr: Node2D | null = null;
+    private __hLine1: Node2D | null = null;
+    private __hLine2: Node2D | null = null;
+    private __vLine1: Node2D | null = null;
+    private __vLine2: Node2D | null = null;
     declare node: Node2D;
     protected onLoad(): void {
         const ctrlMaterial = new SolidColorMaterial();
@@ -18,16 +22,27 @@ export class ClippingFrame extends EngineScript {
         this.__leftBottomCtr = new Node2D('left-bottom');
         this.__rightBottomCtr = new Node2D('right-bottom');
 
+        this.__hLine1 = new Node2D('h-line1', { anchorX: 0 });
+        this.__hLine2 = new Node2D('h-line2', { anchorX: 0 });
+        this.__vLine1 = new Node2D('v-line1', { anchorX: 0 });
+        this.__vLine2 = new Node2D('v-line2', { anchorX: 0 });
+
         this.__addScript(this.__leftTopCtr, ctrlMaterial);
         this.__addScript(this.__rightTopCtr, ctrlMaterial);
         this.__addScript(this.__leftBottomCtr, ctrlMaterial);
         this.__addScript(this.__rightBottomCtr, ctrlMaterial);
 
+        this.__addScript(this.__hLine1, ctrlMaterial);
+        this.__addScript(this.__hLine2, ctrlMaterial);
+        this.__addScript(this.__vLine1, ctrlMaterial);
+        this.__addScript(this.__vLine2, ctrlMaterial);
+
         this.node.addChildren(
             this.__leftTopCtr,
             this.__rightTopCtr,
             this.__leftBottomCtr,
-            this.__rightBottomCtr
+            this.__rightBottomCtr,
+            this.__hLine1
         );
         this.__bindEvents();
     }
@@ -53,29 +68,38 @@ export class ClippingFrame extends EngineScript {
     }
 
     protected onUpdate() {
-        if (this.__leftTopCtr) {
-            this.__leftTopCtr.x =
-                this.node.x - this.node.anchorX * this.node.width;
-            this.__leftTopCtr.y =
-                this.node.y + (1 - this.node.anchorY) * this.node.height;
+        if (
+            !this.__leftTopCtr ||
+            !this.__rightBottomCtr ||
+            !this.__rightTopCtr ||
+            !this.__leftBottomCtr ||
+            !this.__hLine1 ||
+            !this.__hLine2 ||
+            !this.__vLine1 ||
+            !this.__vLine2
+        ) {
+            return;
         }
-        if (this.__rightTopCtr) {
-            this.__rightTopCtr.x =
-                this.node.x + (1 - this.node.anchorX) * this.node.width;
-            this.__rightTopCtr.y =
-                this.node.y + (1 - this.node.anchorY) * this.node.height;
-        }
-        if (this.__leftBottomCtr) {
-            this.__leftBottomCtr.x =
-                this.node.x - this.node.anchorX * this.node.width;
-            this.__leftBottomCtr.y =
-                this.node.y - this.node.anchorY * this.node.height;
-        }
-        if (this.__rightBottomCtr) {
-            this.__rightBottomCtr.x =
-                this.node.x + (1 - this.node.anchorX) * this.node.width;
-            this.__rightBottomCtr.y =
-                this.node.y - this.node.anchorY * this.node.height;
-        }
+        this.__leftTopCtr.x = this.node.x - this.node.anchorX * this.node.width;
+        this.__leftTopCtr.y =
+            this.node.y + (1 - this.node.anchorY) * this.node.height;
+        this.__rightTopCtr.x =
+            this.node.x + (1 - this.node.anchorX) * this.node.width;
+        this.__rightTopCtr.y =
+            this.node.y + (1 - this.node.anchorY) * this.node.height;
+
+        this.__leftBottomCtr.x =
+            this.node.x - this.node.anchorX * this.node.width;
+        this.__leftBottomCtr.y =
+            this.node.y - this.node.anchorY * this.node.height;
+        this.__rightBottomCtr.x =
+            this.node.x + (1 - this.node.anchorX) * this.node.width;
+        this.__rightBottomCtr.y =
+            this.node.y - this.node.anchorY * this.node.height;
+
+        this.__hLine1.x = this.__leftTopCtr.x;
+        this.__hLine1.y = this.__leftTopCtr.y;
+        this.__hLine1.width = this.__rightTopCtr.x - this.__leftTopCtr.x;
+        this.__hLine1.height = DEFAULT_LINE_WIDTH;
     }
 }
